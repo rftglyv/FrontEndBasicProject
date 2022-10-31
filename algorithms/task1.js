f45c05572a6b6abad8d173ca7dbc7e45a48eb798
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2022-11-01 01:45:11 +0400

    Fixed

--- algorithms/task1.js
+++ algorithms/task1.js
@@ -2,13 +2,12 @@ let string ="Any fool can write code that a computer can understand. Good progra
 
 function findStringWordLength(str){
     // daxil edilən str string məlumatının neçə xarakterdən ibarət olduğunu return edən metod yazın
-    const arrOfStrings = str.split(" ")
-    return arrOfStrings.length
+    return str.match(/[ .]/g).length
 }
 
 function findStringLetterLength(str){
     // daxil edilən str string məlumatının neçə hərfdən ibarət olduğunu return edən metod yazın
-    return str.length
+    return str.split(".").join(" ").split(" ").join("").length
 }
 
-console.log(`String ${findStringWordLength(string)} sözdən və ${findStringLetterLength(string)} hərfdən ibarətdir.`)
+console.log(`String ${findStringWordLength(string)} character'dən və ${findStringLetterLength(string)} hərfdən ibarətdir.`)
