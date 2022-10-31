38495efe83f41952972dd390fde7c89bc1ade2ab
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2022-10-31 04:42:12 +0400

    Task Done

---
+++ algorithms/task1.js
@@ -0,0 +1,12 @@
+let word="Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+
+function findStringLength(str){
+    // daxil edilən str string məlumatının neçə xarakterdən ibarət olduğunu return edən metod yazın
+}
+
+function findStringLength(str){
+    // daxil edilən str string məlumatının neçə hərfdən ibarət olduğunu return edən metod yazın
+    return console.log(str.length)
+}
+
+findStringLength(word)
